be7616f1896f4ba0eb2f2311747ed7cb9158a1ee
Bryan Whitehead <driver@megahappy.net>
2026-08-13 12:31:46 -1000

    cli-toot 1.0.2 (new formula)

---
+++ Formula/cli-toot.rb
@@ -0,0 +1,26 @@
+class CliToot < Formula
+  desc "Minimal C23 CLI Mastodon client"
+  homepage "https://github.com/jiqiren/cli-toot"
+  url "https://github.com/jiqiren/cli-toot/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "48e03abbbaef53a3a547bc555ba626071d68e8c44546eb83b3fb63f78db6b374"
+  license "BSD-3-Clause"
+
+  depends_on "meson" => :build
+  depends_on "pkgconf" => :build
+  depends_on "curl"
+  depends_on "cjson"
+
+  on_linux do
+    depends_on "openssl@3"
+  end
+
+  def install
+    system "meson", "setup", "build", *std_meson_args
+    system "meson", "compile", "-C", "build"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    assert_match "cli-toot #{version}", shell_output("#{bin}/cli-toot version")
+  end
+end
