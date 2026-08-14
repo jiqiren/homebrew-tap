98bb3b62fb92b41e5bd992b77c8bc1e059e2054f
Bryan Whitehead <driver@megahappy.net>
2026-08-13 23:33:08 -1000

    cli-toot 1.1.1
    Fix(oauth): send x-www-form-urlencoded to token endpoint. The previous
    multipart/form-data POST was rejected by Mastodon's /oauth/token, breaking
    brew installations (could not authenticate). Bump to v1.1.1.
    
    Closes #12.
    
    Signed-off-by: Bryan Whitehead <driver@megahappy.net>

--- Formula/cli-toot.rb
+++ Formula/cli-toot.rb
@@ -1,21 +1,14 @@
 class CliToot < Formula
   desc "Minimal C23 CLI Mastodon client"
   homepage "https://github.com/jiqiren/cli-toot"
-  url "https://github.com/jiqiren/cli-toot/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "6da391b0d053418823ddada8355139a9cb85c621275a96c8b4bc52c757ee786e"
+  url "https://github.com/jiqiren/cli-toot/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "db8d54fbe83ff7a7ebf71f0d9f5a5134d4dc3cf4a7c137378246a1e0dbb51a5f"
   license "BSD-3-Clause"
   head "https://github.com/jiqiren/cli-toot.git", branch: "main"
 
   livecheck do
     url :stable
     strategy :github_latest
-  end
-
-  bottle do
-    root_url "https://github.com/jiqiren/homebrew-tap/releases/download/cli-toot-1.1.0"
-    sha256 cellar: :any, arm64_tahoe:  "d6d3494f70515741c8714abbb5932569a8553416f4d222c3c7d79aa0b75a8fba"
-    sha256               arm64_linux:  "9f2a14f13f607c1ace046deae88cafb0eb483e37f0108ea1be038dfb247b81ed"
-    sha256               x86_64_linux: "4b6684b22cac6a1fa039f60b47cf05f32f7e34f8c8555852fa6a5b8ab77e0664"
   end
 
   depends_on "meson" => :build
