ff9fe1c6f491b7d3141914894b36ba4f2ef87722
Bryan Whitehead <driver@megahappy.net>
2026-08-20 19:18:36 -1000

    sloptoot: update to 1.3.1
    Adds view -m/--mobile: cached posts are served from the local cache
    with no network; uncached posts are fetched and stored in the cache.
    https://github.com/jiqiren/sloptoot/compare/v1.3.0...v1.3.1

--- Formula/sloptoot.rb
+++ Formula/sloptoot.rb
@@ -1,8 +1,8 @@
 class Sloptoot < Formula
   desc "Minimal C23 CLI Mastodon client"
   homepage "https://github.com/jiqiren/sloptoot"
-  url "https://github.com/jiqiren/sloptoot/archive/refs/tags/v1.3.0.tar.gz"
-  sha256 "cafe69ec367141495c222328558d69655b89988e19b5362467f7a5c60210a5cb"
+  url "https://github.com/jiqiren/sloptoot/archive/refs/tags/v1.3.1.tar.gz"
+  sha256 "585e04c29930cd9337f7b2e22bc60748e27786029e3669c71ab38e4b66a58b93"
   license "BSD-3-Clause"
   head "https://github.com/jiqiren/sloptoot.git", branch: "main"
 
@@ -11,12 +11,6 @@ class Sloptoot < Formula
     strategy :github_latest
   end
 
-  bottle do
-    root_url "https://github.com/jiqiren/homebrew-tap/releases/download/sloptoot-1.3.0"
-    sha256 cellar: :any, arm64_tahoe:  "5ee1c439487f2cdd66fa4ff8d92aa9cee97f6eae2b0228651697a8bc02dd3310"
-    sha256               arm64_linux:  "268f11340aae0b21a2e400b4a1d9ef0fbfdef5bee9bdc5b696f425110235035c"
-    sha256               x86_64_linux: "d7d786cfcd0dbd566b41233c0be43c36b011e85bf2f8257c302a4a497d5b45e2"
-  end
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
