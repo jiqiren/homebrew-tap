8b2702a5dfa9114af4c54442a89178cc7d869ddf
Bryan Whitehead <driver@megahappy.net>
2026-09-19 00:06:23 -1000

    sloptoot 1.4.5
    Skip the kitty graphics probe on unsupported terminals and show wrapped ALT text in kitty graphics mode.
    
    Closes #22.
    
    Signed-off-by: Bryan Whitehead <driver@megahappy.net>

--- Formula/sloptoot.rb
+++ Formula/sloptoot.rb
@@ -1,21 +1,14 @@
 class Sloptoot < Formula
   desc "Minimal C23 CLI Mastodon client"
   homepage "https://github.com/jiqiren/sloptoot"
-  url "https://github.com/jiqiren/sloptoot/archive/refs/tags/v1.4.3.tar.gz"
-  sha256 "fbf3732eca514d6defd3103cb7882c3d88e5c01728957365e2f39b67c22703f7"
+  url "https://github.com/jiqiren/sloptoot/archive/refs/tags/v1.4.5.tar.gz"
+  sha256 "5ec36ff9f6ff9b363ea460e5e5da7cc97c90c01af3f5d17c531566e9e34740a9"
   license "BSD-3-Clause"
   head "https://github.com/jiqiren/sloptoot.git", branch: "main"
 
   livecheck do
     url :stable
     strategy :github_latest
-  end
-
-  bottle do
-    root_url "https://github.com/jiqiren/homebrew-tap/releases/download/sloptoot-1.4.3"
-    sha256 cellar: :any, arm64_tahoe:  "eedaa12bb446184087ee65f9fbd720d5bfef0135284c01ba0aca921aec9acd47"
-    sha256 cellar: :any, arm64_linux:  "1be0e134ae9d9db732054aa99b7a08dfd82734ae7418bd24a0dba99726bdc881"
-    sha256 cellar: :any, x86_64_linux: "574974dcf0e983e81e074d8c832b8ae865d1ca5d39fc24b40fd2d41b57f8fb3d"
   end
 
   depends_on "meson" => :build
